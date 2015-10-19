e73f4ca10c689924665e5bd4dd3adf59c733c88c
ruthysq <karlasimpson@macbook-pro.home>
2015-10-19 21:44:24 +0100

    Finance Site

---
+++ js/ruthfinance.js
@@ -0,0 +1,17 @@
+// external JS: masonry.pkgd.js
+
+$(document).ready( function() {
+
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: 160
+  });
+
+  $grid.on( 'click', '.grid-item', function() {
+    // change size of item via class
+    $( this ).toggleClass('grid-item--gigante');
+    // trigger layout
+    $grid.masonry();
+  });
+
+});
